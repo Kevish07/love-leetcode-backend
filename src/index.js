be5bb2ecca6afe871b380f188f2530dbbe195820
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-06-06 17:33:01 +0530

    no cors

--- src/index.js
+++ src/index.js
@@ -16,11 +16,12 @@ dotenv.config({
 
 const port = process.env.PORT
 const app = express()
-app.use(cors({
-    // origin: process.env.FRONTEND_URL,
-    origin: "*",
-    credentials: true
-}))
+// app.use(cors({
+//     // origin: process.env.FRONTEND_URL,
+//     origin: "*",
+//     credentials: true
+// }))
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
