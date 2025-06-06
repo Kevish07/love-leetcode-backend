6a5db8f35cf0b0decdede844710a3b511863b544
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-06-06 17:28:01 +0530

    cors issue

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ dotenv.config({
 const port = process.env.PORT
 const app = express()
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    // origin: process.env.FRONTEND_URL,
+    origin: "*",
     credentials: true
 }))
 app.use(express.json())
