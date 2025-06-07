90904163c1e4850b08b8eb83bd16b5db1819929c
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-06-07 16:33:34 +0530

    cors error

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import { ApiResponse } from "./utils/api-response.js"
-import authRoutes from "./routes/auth.routes.js"
-import problemRoutes from "./routes/problem.routes.js"
-import executionRoutes from "./routes/executionCode.routes.js"
-import submissionRoutes from "./routes/submission.routes.js"
-import playlistRoutes from "./routes/playlist.routes.js"
+import { ApiResponse } from "./utils/api-response.js";
+import authRoutes from "./routes/auth.routes.js";
+import problemRoutes from "./routes/problem.routes.js";
+import executionRoutes from "./routes/executionCode.routes.js";
+import submissionRoutes from "./routes/submission.routes.js";
+import playlistRoutes from "./routes/playlist.routes.js";
 
 dotenv.config({
-    path:"./.env"
-})
+  path: "./.env",
+});
 
-const port = process.env.PORT
-const app = express()
+const port = process.env.PORT;
+const app = express();
 // app.use(cors({
 //     // origin: process.env.FRONTEND_URL,
 //     origin: "*",
@@ -23,7 +23,7 @@ const app = express()
 // }))
 
 const corsOptions = {
-  origin: "https://www.loveleetcode.in/", // replace with your frontend origin
+  origin: "https://www.loveleetcode.in", // replace with your frontend origin
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
@@ -31,21 +31,21 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.status(200).json(new ApiResponse(200, "Sever is running"));
+});
 
-app.get("/",(req,res)=>{
-    res.status(200).json(new ApiResponse(200,"Sever is running"));
-})
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes);
+app.use("/api/v1/execution-code", executionRoutes);
+app.use("/api/v1/submission", submissionRoutes);
+app.use("/api/v1/playlist", playlistRoutes);
 
-app.use("/api/v1/auth",authRoutes)
-app.use("/api/v1/problems",problemRoutes)
-app.use("/api/v1/execution-code",executionRoutes)
-app.use("/api/v1/submission",submissionRoutes)
-app.use("/api/v1/playlist",playlistRoutes)
-
-
-app.listen(port,()=>{{
-    console.log(`Server is running on port: ${port}`)
-}})
+app.listen(port, () => {
+  {
+    console.log(`Server is running on port: ${port}`);
+  }
+});
